ff01c5e022009573b91cb9df30ce06ae71cfc41a
M.Rosa <rosa1522@hotmail.com>
2011-02-12 18:25:41 +0100

    Llista.

--- src/cat/urv/clic/android/LlistaClics.java
+++ src/cat/urv/clic/android/LlistaClics.java
@@ -1,13 +1,31 @@
 package cat.urv.clic.android;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class LlistaClics extends Activity {
+
+public class LlistaClics extends ListActivity {
     /** Called when the activity is first created. */
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.llista);
+//    }
+    
+    
+    private Object[] fruits = {"Orange", "Apple", "Pear"}; 
+    private ArrayAdapter<TextView> adp = null; 
+    
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.llista);
+    	
+    	adp = new ArrayAdapter(this,R.id.listItem,fruits); 
+    	setListAdapter(adp); 
+    	setContentView(R.layout.llista); 
     }
+    
 }
