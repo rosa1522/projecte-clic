a4d0b26cf9b2ed4149dd12d0595c1904280ce410
Aina <aina07@gmail.com>
2011-03-15 20:32:22 +0100

    Pantalla Descripció

--- src/cat/urv/clic/android/IniciAplicacio.java
+++ src/cat/urv/clic/android/IniciAplicacio.java
@@ -61,9 +61,9 @@ public class IniciAplicacio extends Activity implements OnClickListener{
 	        intent = new Intent(this, DescripcioJoc.class);	
 	        OnItemClickListener onClic = new OnItemClickListener(){
 	        						public void onItemClick(AdapterView<?> arg0, View v, int i, long id) {
-	        							String str = list.getItemAtPosition(i).toString();
+	        							Integer str = (Integer) list.getItemAtPosition(i);
 	        					        							
-	        							DescripcioJoc.name = str;
+	        							DescripcioJoc.identificador = str;
 	        							startActivity(intent);	        		
 	        						}};
 	        list.setOnItemClickListener(onClic);
