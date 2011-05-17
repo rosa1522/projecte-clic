31f20cd6b6c0539a5b4ef9fee5df1f0f649b35e4
Jordi Duch <jordi.duch@gmail.com>
2011-05-17 19:30:38 +0200

    Canvis per a que funcioni en android

---
+++ assets/prova/data.js
@@ -0,0 +1,363 @@
+var maxActivitats = 4;
+
+var dadesActivitat = {"settings": {
+				        "atributsActivitat": {
+				            "skin-file": "@orange.xml",
+				            "organization-state": "Catalunya",
+				            "title": "La xocolata",
+				            "descriptors-level": "Infantil (3-6)",
+				            "organization-name": "CEIP La Florida",
+				            "language": "català",
+				            "organization-mail": "a8028436@centres.xtec.es",
+				            "author-name": "Maite Álvarez Leo",
+				            "descriptors-area": "Diversos",
+				            "organization-city": "Santa Perpètua de Mogoda",
+				            "organization-url": "http://www.xtec.es/centres/a8028436",
+				            "author-mail": "malva232@xtec.cat"
+				        },
+				        "descripcio": ["Projecte d\u0027activitats per a educació infantil sobre la xocolata. A partir 		d\u0027aquest centre d\u0027interès trobem activitats de matemàtiques, com ordenar 		sèries; 	de llegües, com ordenar lletres, relacionar sons amb paraules escrites, etc..."],
+				        "index": 1
+					    },
+					    "activitats": [{
+						    	"atributsActivitat": {
+						            "settings-window-border": "true",
+						            "settings-window-bgColor": "0xFF6600",
+						            "settings-container-counters-score": "true",
+						            "settings-skin-file": "@orange.xml",
+						            "classe": "@panels.InformationScreen",
+						            "settings-container-image-tiled": "true",
+						            "code": "",
+						            "settings-report": "false",
+						            "settings-margin": "8",
+						            "name": "Inici",
+						            "settings-container-counters-actions": "true",
+						            "settings-container-image-name": "dibuix pirata.jpg",
+						            "settings-container-counters-time": "true",
+						            "settings-container-bgColor": "0xC0C0C0"
+						        },
+						        "cell": [{
+						            "atributs": {
+						                "style-font-family": "Arial",
+						                "p": "LA SELVA I EL GEL FAN DE PIRATES",
+						                "style-color-background": "0x3366FF",
+						                "style-color-foreground": "0xFFFFFF",
+						                "style-font-size": "22",
+						                "style-borderStroke": "0.7",
+						                "type": "initial",
+						                "style-markerStroke": "5.0",
+						                "style-font-bold": "true",
+						                "media-file": "AMB_SN_5.mp3",
+						                "media-type": "PLAY_AUDIO"
+						            }
+						        }],
+						        "celllist": [{
+						            "atributs": {
+						                "shaper-rows": "1",
+						                "cols": "1",
+						                "style-font-family": "Arial",
+						                "shaper-cols": "1",
+						                "cellHeight": "219.0",
+						                "style-font-size": "36",
+						                "style-gradient-angle": "85",
+						                "style-gradient-source": "0x00CCCC",
+						                "style-font-bold": "true",
+						                "cellWidth": "303.0",
+						                "rows": "1",
+						                "style-color-border": "0x0000CC",
+						                "shaper-class": "@Rectangular",
+						                "style-color-background": "0x00CCCC",
+						                "style-color-foreground": "0xCC0000",
+						                "style-borderStroke": "3.0",
+						                "border": "true",
+						                "style-gradient-dest": "0xFFFFFF",
+						                "style-markerStroke": "5.0"
+						            },
+						            "cell": [{
+						                "atributs": {
+						                    "txtAlign": "middle,bottom",
+						                    "image": "lloro gif.gif",
+						                    "imgAlign": "middle,top",
+						                    "p": "PIRATES"
+						                }
+						            }]
+						        }]
+					    		},{
+					    			"atributsActivitat": {
+					    		        "settings-window-border": "true",
+					    		        "settings-window-bgColor": "0xFFFFFF",
+					    		        "settings-container-gradient-dest": "0xFFFFFF",
+					    		        "settings-container-counters-score": "true",
+					    		        "settings-container-gradient-angle": "108",
+					    		        "classe": "@panels.InformationScreen",
+					    		        "code": "",
+					    		        "settings-report": "false",
+					    		        "settings-margin": "8",
+					    		        "settings-container-gradient-source": "0x993300",
+					    		        "name": "inici",
+					    		        "settings-container-counters-actions": "true",
+					    		        "settings-container-counters-time": "true",
+					    		        "settings-container-bgColor": "0xC0C0C0"
+					    		    },
+
+					    		    "cell": [{
+					    		        "atributs": {
+					    		            "media-level": "1",
+					    		            "style-font-family": "Astigma",
+					    		            "p": "LA XOCOLATA",
+					    		            "style-color-background": "0x993300",
+					    		            "style-color-foreground": "0xFFFFFF",
+					    		            "style-font-size": "50",
+					    		            "media-type": "PLAY_AUDIO",
+					    		            "type": "initial",
+					    		            "media-file": "la xocolata.mp3"
+					    		        }
+					    		    }],
+
+					    		    "celllist": [{
+					    		        "atributs": {
+					    		            "shaper-rows": "1",
+					    		            "cols": "1",
+					    		            "shaper-cols": "1",
+					    		            "shaper-class": "@Rectangular",
+					    		            "cellHeight": "312.0",
+					    		            "border": "true",
+					    		            "cellWidth": "336.0",
+					    		            "rows": "1"
+					    		        },
+					    		        "cell": [{
+					    		            "atributs": {
+					    		                "image": "FOTO CACAU copia [640x480].jpg"
+					    		            }
+					    		        }]
+					    		    }]
+					    		},{
+					    			
+								"atributsActivitat": {
+							        "settings-window-border": "true",
+							        "settings-window-bgColor": "0xFF6600",
+							        "settings-container-gradient-dest": "0xFFFFFF",
+							        "settings-container-counters-score": "true",
+							        "settings-container-gradient-angle": "269",
+							        "settings-skin-file": "@orange.xml",
+							        "classe": "@puzzles.ExchangePuzzle",
+							        "settings-dragCells": "true",
+							        "code": "",
+							        "settings-report": "true",
+							        "settings-margin": "8",
+							        "scramble-times": "31",
+							        "settings-container-gradient-source": "0xFFFF66",
+							        "name": "Puzzle",
+							        "settings-container-counters-actions": "true",
+							        "settings-reportActions": "false",
+							        "settings-container-counters-time": "true",
+							        "settings-container-bgColor": "0xFFFF66"
+							    },
+							    "cell": [{
+							        "atributs": {
+							            "media-level": "1",
+							            "style-color-border": "0x330033",
+							            "style-font-family": "Arial",
+							            "p": "QUÈ ÉS?",
+							            "style-color-background": "0x99FF66",
+							            "style-color-foreground": "0x330033",
+							            "style-font-size": "22",
+							            "media-type": "PLAY_AUDIO",
+							            "style-borderStroke": "1.4",
+							            "type": "initial",
+							            "media-file": "Que es pirates.mp3",
+							            "style-markerStroke": "2.7",
+							            "style-font-bold": "true"
+							        }
+							    }, {
+							        "atributs": {
+							            "style-color-border": "0xFF66FF",
+							            "style-font-family": "Arial",
+							            "p": "UN VAIXELL!",
+							            "style-color-background": "0x99FF66",
+							            "style-color-foreground": "0x330033",
+							            "style-font-size": "22",
+							            "style-borderStroke": "2.0",
+							            "type": "final",
+							            "style-markerStroke": "2.7",
+							            "style-font-bold": "true"
+							        }
+							    }],
+							    "celllist": [{
+							        "atributs": {
+							            "shaper-rows": "2",
+							            "image": "VAIXELL-resized.jpg",
+							            "cols": "3",
+							            "shaper-cols": "3",
+							            "shaper-class": "@Rectangular",
+							            "border": "true",
+							            "rows": "2"
+							        },
+							        "cell": []
+							    }]
+					    	},{
+					    		"atributsActivitat": {
+					                "settings-window-border": "true",
+					                "settings-window-bgColor": "0xFF9933",
+					                "settings-container-gradient-dest": "0xFFFFFF",
+					                "settings-container-counters-score": "true",
+					                "layout-position": "AUB",
+					                "settings-container-gradient-angle": "269",
+					                "settings-skin-file": "@orange.xml",
+					                "classe": "@memory.MemoryGame",
+					                "code": "",
+					                "settings-report": "true",
+					                "settings-margin": "8",
+					                "scramble-times": "31",
+					                "settings-container-gradient-source": "0xFFFF66",
+					                "name": "Joc de memÃ²ria",
+					                "settings-container-counters-actions": "true",
+					                "settings-reportActions": "false",
+					                "settings-container-counters-time": "true",
+					                "settings-container-bgColor": "0xFFFF66"
+					            },
+					            "cell": [{
+					                "atributs": {
+					                    "media-level": "1",
+					                    "style-font-family": "Arial Black",
+					                    "style-color-alternative": "0xCCCCCC",
+					                    "style-font-size": "22",
+					                    "media-type": "PLAY_AUDIO",
+					                    "type": "initial",
+					                    "media-file": "Trob les parelles.mp3",
+					                    "style-font-bold": "true",
+					                    "style-color-inactive": "0xFFFF99",
+					                    "style-color-border": "0x99FF33",
+					                    "p": "TROBA LES PARELLES",
+					                    "style-color-background": "0xFFFF99",
+					                    "style-color-foreground": "0xCC0000",
+					                    "style-borderStroke": "3.0",
+					                    "border": "true",
+					                    "style-markerStroke": "2.7"
+					                }
+					            }, {
+					                "atributs": {
+					                    "media-level": "1",
+					                    "style-font-family": "Arial Black",
+					                    "style-font-size": "22",
+					                    "media-type": "PLAY_AUDIO",
+					                    "type": "final",
+					                    "media-file": "Fantastic.mp3",
+					                    "style-font-bold": "true",
+					                    "style-color-border": "0x99FF33",
+					                    "p": "FANTÃ€STIC!!",
+					                    "style-color-background": "0xFFFF99",
+					                    "style-color-foreground": "0xCC0000",
+					                    "style-borderStroke": "2.2",
+					                    "border": "true",
+					                    "style-markerStroke": "2.7"
+					                }
+					            }],
+					            "celllist": [{
+					                "atributs": {
+					                    "shaper-rows": "1",
+					                    "cols": "6",
+					                    "style-font-family": "Arial",
+					                    "shaper-cols": "6",
+					                    "cellHeight": "157.0",
+					                    "style-font-size": "17",
+					                    "style-font-bold": "true",
+					                    "cellWidth": "205.0",
+					                    "rows": "1",
+					                    "style-color-inactive": "0xCCFF66",
+					                    "id": "primary",
+					                    "style-color-border": "0xCC0000",
+					                    "shaper-class": "@Rectangular",
+					                    "style-color-background": "0xFFFFFF",
+					                    "style-borderStroke": "2.4",
+					                    "border": "true",
+					                    "style-markerStroke": "2.7"
+					                },
+					                "cell": [{
+					                    "atributs": {
+					                        "image": "pirata2[1].jpg"
+					                    }
+					                }, {
+					                    "atributs": {
+					                        "image": "pirata1[1].png"
+					                    }
+					                }, {
+					                    "atributs": {
+					                        "image": "espada_pirata_63_cm_CM035LI_1[1].jpg"
+					                    }
+					                }, {
+					                    "atributs": {
+					                        "image": "dibuix pirata.jpg"
+					                    }
+					                }, {
+					                    "atributs": {
+					                        "image": "barret.jpg"
+					                    }
+					                }, {
+					                    "atributs": {
+					                        "image": "GarfioPirata[1].jpg"
+					                    }
+					                }]
+					            }]
+						    },{
+						    "atributsActivitat": {
+					            "settings-window-border": "true",
+					            "settings-window-bgColor": "0x0000FF",
+					            "settings-container-gradient-dest": "0x6666FF",
+					            "settings-container-counters-score": "true",
+					            "layout-position": "AB",
+					            "settings-container-gradient-angle": "110",
+					            "settings-skin-file": "@orange.xml",
+					            "classe": "@puzzles.DoublePuzzle",
+					            "settings-dragCells": "true",
+					            "code": "",
+					            "settings-report": "true",
+					            "settings-margin": "8",
+					            "scramble-times": "31",
+					            "settings-container-gradient-source": "0x0000CC",
+					            "name": "puzzle campana",
+					            "settings-container-counters-actions": "true",
+					            "settings-reportActions": "false",
+					            "settings-container-counters-time": "true",
+					            "settings-container-bgColor": "0xC0C0C0"
+					        },
+					        "cell": [{
+					            "atributs": {
+					                "media-level": "1",
+					                "style-font-family": "Arial Black",
+					                "p": "QUÈ ÉS?",
+					                "style-color-background": "0x6633FF",
+					                "style-font-size": "48",
+					                "media-type": "PLAY_AUDIO",
+					                "type": "initial",
+					                "media-file": "que es.mp3"
+					            }
+					        }, {
+					            "atributs": {
+					                "media-level": "1",
+					                "style-font-family": "Arial Black",
+					                "p": "UNA CAMPANA DE XOCOLATA!!",
+					                "style-color-background": "0x0099FF",
+					                "style-font-size": "48",
+					                "media-type": "PLAY_AUDIO",
+					                "type": "final",
+					                "media-file": "una campana de xocolata.mp3"
+					            }
+					        }],
+					        "celllist": [{
+					            "atributs": {
+					                "style-color-inactive": "0x66CCFF",
+					                "shaper-rows": "3",
+					                "style-color-border": "0xFFFF00",
+					                "cols": "3",
+					                "shaper-cols": "3",
+					                "shaper-class": "@ClassicJigSaw",
+					                "image": "IMG_8303 [640x480].JPG",
+					                "style-borderStroke": "0.7",
+					                "border": "true",
+					                "style-markerStroke": "2.7",
+					                "rows": "3"
+					            },
+					            "cell": []
+					        }]
+					}]
+};
